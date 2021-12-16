f1d43b63bf7c2a3efc7816c4f5f15a457b4fdad7
seijiro <modeverv@gmail.com>
2021-12-16 14:00:24 +0900

    2021-12-16 14:00:24

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,44 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+          home: Scaffold(
+        appBar: AppBar(title: Text('8-Ball')),
+        body: EightBall(),
+      )),
+    );
+
+class EightBall extends StatelessWidget {
+//  const EightBall({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BallPage();
+  }
+}
+
+class BallPage extends StatefulWidget {
+  @override
+  _BallPageState createState() => _BallPageState();
+}
+
+class _BallPageState extends State<BallPage> {
+  int imgNum = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: FlatButton(
+          child: Image.asset("images/ball$imgNum.png"),
+          onPressed: () => {
+            setState(() {
+              imgNum = Random().nextInt(5) + 1;
+            })
+          },
+        ),
       ),
     );
+  }
+}
